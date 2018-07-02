0819990b078dc4aa7178a8370e1f6188d8063c51
Nightingale-UA <36628451+Nightingale-UA@users.noreply.github.com>
2018-07-02 09:40:26 +0300

    Update Swapfuck.java

--- src/interpreter/Swapfuck.java
+++ src/interpreter/Swapfuck.java
@@ -5,34 +5,34 @@ import bf.cls.Interpreter;
 import java.util.stream.Collectors;
 
 public class Swapfuck implements Interpreter {
-	private String op = "@><[].";
+    private String op = "@><[].";
 	
-	public String interpret(String code, Deque<Integer> input) {
-		StringBuilder output = new StringBuilder();
-		List<Integer> tape = new ArrayList<>();
+    public String interpret(String code, Deque<Integer> input) {
+	StringBuilder output = new StringBuilder();
+	List<Integer> tape = new ArrayList<>();
 		
-		Deque<Integer> inputBits = new ArrayDeque<>();
-		input.forEach(i -> fillByte(Integer.toBinaryString(i))
-					  				.chars().forEach(c -> inputBits.add(c - 48)));
-		tape.addAll(inputBits);
+	Deque<Integer> inputBits = new ArrayDeque<>();
+	input.forEach(i -> fillByte(Integer.toBinaryString(i))
+					   .chars().forEach(c -> inputBits.add(c - 48)));
+	tape.addAll(inputBits);
 		
-		Deque<Integer> outputBits = new ArrayDeque<>();
-		int swapReg = -1;
-		int pointer = 0;
+	Deque<Integer> outputBits = new ArrayDeque<>();
+	int swapReg = -1;
+	int pointer = 0;
         int length = code.length();
 		
-		for (int i = 0; i < length; i++) {
-			switch (code.charAt(i)) {
-				case '@':
-					if (swapReg == -1) swapReg = pointer;
-					else {
-						int temp = tape.get(swapReg);
-						tape.set(swapReg, tape.get(pointer));
-						tape.set(pointer, temp);
-						swapReg = -1;
-					}
-					break;
-				case '>':
+	for (int i = 0; i < length; i++) {
+	    switch (code.charAt(i)) {
+		case '@':
+		    if (swapReg == -1) swapReg = pointer;
+		    else {
+			int temp = tape.get(swapReg);
+			tape.set(swapReg, tape.get(pointer));
+			tape.set(pointer, temp);
+			swapReg = -1;
+		    }
+		    break;
+		case '>':
                     pointer++;
                     if (pointer == tape.size()) {
                         tape.add(0);
@@ -47,12 +47,12 @@ public class Swapfuck implements Interpreter {
                     break;                
                 case '.':
                     outputBits.addFirst(tape.get(pointer));
-					if (outputBits.size() == 8) {
-						output.append((char)Integer.parseInt(outputBits.stream()
-														 		   .map(n -> String.valueOf(n))
-														  		   .collect(Collectors.joining()),2));
-						outputBits.clear();
-					}
+		    if (outputBits.size() == 8) {
+			output.append((char)Integer.parseInt(outputBits.stream()
+							     	       .map(n -> String.valueOf(n))
+								       .collect(Collectors.joining()),2));
+		    outputBits.clear();
+		    }
                     break;
                 case '[':
                     if (tape.get(pointer) == 0) {
